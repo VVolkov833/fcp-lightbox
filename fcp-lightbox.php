272eff54c1c95189819a6689f5760c9caa17e004
VVolkov833 <finnish.ru@gmail.com>
2022-08-20 00:52:50 +0300

    improved working with preferences / $settings

--- fcp-lightbox.php
+++ fcp-lightbox.php
@@ -14,7 +14,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'FCPLB_DEV', false );
+define( 'FCPLB_DEV', true );
 define( 'FCPLB_VER', get_file_data( __FILE__, [ 'ver' => 'Version' ] )[ 'ver' ] . ( FCPLB_DEV ? time() : '' ) );
 
 
@@ -29,7 +29,8 @@ add_action( 'wp_enqueue_scripts', function() {
     $translations = array_map( function( $a ) use ( $translations ) {
         return __( $translations[ $a ] );
     }, array_flip( $translations ) );
-    
+
+    // enqueue the loader script
     wp_enqueue_script( 'fcp-lightbox', $assets_url( 'loader', 'js' ), [], FCPLB_VER );
     add_filter('script_loader_tag', function ($tag, $handle) { // change to async
         if ( $handle !== 'fcp-lightbox' ) { return $tag; }
@@ -37,15 +38,15 @@ add_action( 'wp_enqueue_scripts', function() {
     }, 10, 2);
     
     // global js values
-    wp_add_inline_script( 'fcp-lightbox', preg_replace( '/[\n\t ]+/', '', '
-        window.fcp_lightbox = {
-            "path" : "' . esc_js( plugin_dir_url(__FILE__) ) . '",
-            "dev" : ' . ( FCPLB_DEV ? 'true' : 'false' ) . ',
-            "ver" : "' . esc_js( FCPLB_VER ) . '",
-            "selector" : "a[href$=\".jpg\"], a[href$=\".jpeg\"], a[href$=\".png\"]",
-            "translations" : ' . json_encode( $translations ) . ',
-        };
-    '));
+    $settings = [
+        'path' => esc_js( plugin_dir_url(__FILE__) ),
+        'dev' => FCPLB_DEV ? true : false,
+        'ver' => esc_js( FCPLB_VER ),
+        'selector' => 'a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"]',
+        'translations' => $translations,
+    ];
+    
+    wp_add_inline_script( 'fcp-lightbox', 'window.fcp_lightbox = '.json_encode( $settings ).';' );
     
     // wp_enqueue_style( 'fcp-lightbox', $assets_url( 'style', 'css' ), [], FCPLB_VER ); // moved to script.js to eleminate render blocking
 });
